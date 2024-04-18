1d602e3f9cb10ea795153691525c64f8679df4c3
Aijalon Júnior <juniorsjs6@hotmail.com>
2024-04-17 22:35:43 -0300

    Lista Finalizada

---
+++ MetodosNumericos/src/lista2/Lista2.java
@@ -0,0 +1,158 @@
+package lista2;
+
+public class Lista2 {
+	double tolerancia = 0.00001;
+	double erroAbsoluto = 100;
+	double x = 0;
+	double xold = 0;
+	double fx = 0;
+	double fl = 0;
+	double fu = 0;
+	int i = 0;
+		
+	public void bissecao(int a, int i, double xl, double xu) {
+		
+		fl = funcao(xl, a);
+
+		while(erroAbsoluto>tolerancia && this.i<i) {
+			xold=x;
+			x=(xl+xu)/2;
+			fx = funcao(x, a);
+			i++;
+			if(x!=0) {
+				erroAbsoluto = erroAbsoluto();
+			}
+			
+			if(fl*fx<0) {
+				xu = x;
+			}else {
+				xl = x;
+				fl = fx;
+			}
+			
+			imprimir();
+			
+		}
+		resetarVariaveis();
+	}
+	
+	public void falsaPosicao(int a, int i, double xl, double xu) {
+		
+		fl = funcao(xl, a);
+		fu = funcao(xu, a);
+		
+		if(Math.abs(fl)<Math.abs(fu)) {
+			x = xl;
+		}else {
+			x = xu;
+		}
+		
+		while(erroAbsoluto > tolerancia && this.i<i) {
+			xold = x;
+			i++;
+			x = xu + (fu*(xl-xu))/(fu-fl);
+			fx = funcao(x, a);
+			
+			if(x!=0) {
+				erroAbsoluto = erroAbsoluto();
+			}
+			
+			if(Math.abs(fl)<Math.abs(fu)) {
+				xl = x;
+				fl = funcao(x, a);
+			}else {
+				xu = x;
+				fu = funcao(x, a);
+			}
+
+			imprimir();
+		}
+		resetarVariaveis();
+	}
+	
+	public void pontoFixo(int a, int i) {
+		x++;
+		
+		while(erroAbsoluto>tolerancia && this.i<i) {
+			xold = x;
+			this.i++;
+			x = funcaoG(xold, a);
+			if(x!=0) {
+				erroAbsoluto = erroAbsoluto();
+			}
+			imprimir();
+		}	
+		
+		resetarVariaveis();
+	}
+	
+	public void newtonRaphson(int a, int i) {
+		x++;
+		while(erroAbsoluto>tolerancia && this.i<i) {
+			xold = x;
+			this.i++;
+			x = xold - funcao(xold, a)/funcaoDerivada(xold, a);
+			if(x!=0) {
+				erroAbsoluto = erroAbsoluto();
+			}
+			imprimir();
+		}
+		
+		resetarVariaveis();
+	}
+	
+	public void secante(int a, int i, double xl, double xu) {
+		while(erroAbsoluto>tolerancia && this.i<i) {
+			this.i++;
+			fl = funcao(xl, a);
+			fu = funcao(xu, a);
+			x = xu - fu*(xl-xu)/(fl-fu);
+			
+			if (x!=0) {
+				erroAbsoluto = Math.abs(((x-xu)/x)*100);
+			}
+			
+			xl = xu;
+			xu = x;
+			
+			imprimir();
+		}
+		
+		resetarVariaveis();
+	}
+	
+	
+	private double funcao(double x, int a) {
+		return x/a - Math.tan(x*a);
+	}
+	
+	private double funcaoG(double x, int a) {
+		return a*a*Math.tan(x);
+	}
+	
+	private double funcaoDerivada(double x, int a) {
+		return 1/a - 1*a* Math.sqrt(1/Math.cos(a*x)); 
+	}
+	
+	private void resetarVariaveis() {
+		tolerancia = 0.00001;
+		erroAbsoluto = 100;
+		x = 0;
+		xold = 0;
+		fx = 0;
+		fl = 0;
+		fu = 0;
+		i = 0;
+	}
+	
+	private double erroAbsoluto() {
+		return Math.abs(((x-xold)/x)*100);
+	}
+	
+	private void imprimir() {
+		System.out.println("x:"+x);
+		System.out.println("Fx:"+fx);
+		System.out.println("Erro Absoluto:"+erroAbsoluto);			
+		System.out.println(this.i);
+	}
+}
--- MetodosNumericos/src/lista2/Start.java
+++ MetodosNumericos/src/lista2/Start.java
@@ -3,9 +3,14 @@ package lista2;
 public class Start {
 
 	public static void main(String[] args) {
-		Bissecao bissecao = new Bissecao();
+		Lista2 lista2 = new Lista2();
+//		
+//		lista2.bissecao(1, 4.4, 4.8);
+//		lista2.bissecao(2, 4.4, 4.8);
+		//lista2.falsaPosicao(1, 4.4, 4.8);
+		//lista2.pontoFixo(1, 100000);
+		//lista2.newtonRaphson(1, 100);
 		
-		bissecao.bissecao(1, 4.4, 4.8);
-		bissecao.bissecao(2, 4.4, 4.8);
+		lista2.secante(2, 100, 1, 2);
 	}
 }
